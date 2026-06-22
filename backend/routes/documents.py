c75ec8aa635aa43940b9890423472eaee4e9989a
nidulaj <nidulajayalath@gmail.com>
2026-06-22 12:03:10 +0530

    feat: enhance document upload functionality with indexing and chunking

--- backend/routes/documents.py
+++ backend/routes/documents.py
@@ -3,11 +3,20 @@ from flask import BluePrint, request, jsonify
 from supabase import create_client
 from dotenv import load_dotenv
 
+from app.rag.loader import PDFLoader
+from app.rag.chunker import Chunker
+from app.rag.vecctor_store import VectorStore
+
 load_dotenv()
 
 supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
+vector_store = VectorStore()
+
 document_bp = BluePrint("documents", __name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @document_bp.route("/document/upload", methods=["POST"])
 def upload_document():
@@ -21,6 +30,12 @@ def upload_document():
     
     file_name = file.filename
 
+    local_path = os.path.join(
+        UPLOAD_FOLDER,
+        file_name
+    )
+    file.save(local_path)
+
     supabase.storage \
         .from_("documents") \
         .upload(file_name, file.read())
@@ -29,11 +44,25 @@ def upload_document():
         .from_("documents") \
         .get_public_url(file_name)
 
-    supabase.table("documents").insert({
+    response = supabase.table("documents").insert({
         "name": file_name,
         "file_url": file_url
-    })
+    }) \
+    .execute()
+
+    document_id = response.data[0]["id"]
+
+    text =  PDFLoader.load(local_path)
+
+    chunks = Chunker.split(text)
+
+    vector_store.add_document(
+        doc_id=document_id,
+        chunks=chunks
+    )
     
     return jsonify({
-        "Message": "File uploaded successfully."
+        "message": "Document uploaded and indexed",
+        "document_id": document_id,
+        "chunks": len(chunks)
     })
